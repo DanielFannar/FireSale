8d541d6c2d72ec6308b629093f248b90a94a5f30
DanielFannar <daniel.fannar@gmail.com>
2022-05-03 23:27:47 +0000

    Added views and templates for listings

--- listing/urls.py
+++ listing/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # http://localhost:8000/candies
-    path('', views.index, name="index")
+    # http://localhost:8000/listing
+    path('', views.get_all_listings, name="listings"),
+    path('<int:id>/details', views.get_listing_by_id, name="listing-details")
 ]
--- listing/views.py
+++ listing/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from listing.models import Listing
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse('THIS TEST SUCCEEDED')
+def get_listing_by_id(request, id):
+    return render(request, 'listing/listingdetails.html', {
+        'listing': get_object_or_404(Listing, pk=id)
+    })
+
+
+def get_all_listings(request):
+    return render(request, 'listing/listings.html', {
+    'listings': Listing.objects.all()
+    })
+
+
