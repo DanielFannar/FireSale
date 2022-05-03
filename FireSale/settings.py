2e2df7ba04d6c6dbda51a02d8fbb8087ba9d9a40
DanielFannar <daniel.fannar@gmail.com>
2022-05-03 17:32:09 +0000

    Uncommented DATABASE from settings

--- FireSale/settings.py
+++ FireSale/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'FireSale.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-'''
+
 DATABASES = {
   'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -85,7 +85,7 @@ DATABASES = {
         'HOST': '35.246.89.129',
         'PORT': '5432'
     }
-}'''
+}
 
 
 # Password validation
