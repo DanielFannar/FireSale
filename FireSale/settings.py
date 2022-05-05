5b22f1d0b5a74d273d01dc3dbe36ff7caa2488a6
DanielFannar <daniel.fannar@gmail.com>
2022-05-05 21:49:01 +0000

    moved some settings to local_settings

--- FireSale/settings.py
+++ FireSale/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 from pathlib import Path
 import os
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%s9*p!-dvyp-3kpevf6l15na=a!@r_&f+tyk_(3kfifeevi=h'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,16 +81,7 @@ WSGI_APPLICATION = 'FireSale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'firesale-db',
-        'USER': 'adh-firesale-user',
-        'PASSWORD': 'verklegt2',
-        'HOST': '35.246.89.129',
-        'PORT': '5432'
-    }
-}
+
 
 
 # Password validation
@@ -139,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/user/login'
 LOGIN_REDIRECT_URL = '/'
 
+from .settings_local import *
---
+++ FireSale/settings_local.py
@@ -0,0 +1,12 @@
+DATABASES = {
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firesale-db',
+        'USER': 'adh-firesale-user',
+        'PASSWORD': 'verklegt2',
+        'HOST': '35.246.89.129',
+        'PORT': '5432'
+    }
+}
+
+SECRET_KEY = 'django-insecure-v%s9*p!-dvyp-3kpevf6l15na=a!@r_&f+tyk_(3kfifeevi=h'
