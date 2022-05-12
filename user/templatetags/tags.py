f8fee696830268ed788387e83c47f4da2141b204
DanielFannar <daniel.fannar@gmail.com>
2022-05-12 14:14:53 +0000

    Notification and Message in NavBar, more work done

--- user/templatetags/tags.py
+++ user/templatetags/tags.py
@@ -12,6 +12,9 @@ def number_of_unread_messages(request):
         number_of_unread_messages = ''
     return number_of_unread_messages
 
-
+@register.simple_tag
 def number_of_unread_notifications(request):
-    return Notification.objects.all().filter(recipient_id=request.user.id, seen=False).count()
+    number_of_unread_notifications = Notification.objects.all().filter(recipient_id=request.user.id, seen=False).count()
+    if number_of_unread_notifications == 0:
+        number_of_unread_notifications = ''
+    return number_of_unread_notifications
