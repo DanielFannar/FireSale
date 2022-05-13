221a1aaf40cee55db76c91e8b3f20cc41c5ac493
Hjalmar <hjalmar20@ru.is>
2022-05-13 23:43:09 +0000

    last mianute fix

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FireSale
 Verklegt 2 project by: Arnfinnur Kolbeinsson, Daníel Fannar Guðmundsson, Hjálmar Stefánsson
+
+Þetta er vefmarkaðurinn FireSale. 
+
+Aukakröfur:
+ - Messaging kerfi fyrir notendur.
+ - Statistics síða
+ - Síður fyrir:
+	- Mín listing og Listing frá tilteknum notanda
+	- Mín offer og Offer frá tilteknum notanda
+	- Mín purchases og Purchases frá tilteknum notanda
+- Gullfalleg dansandi forsíðumynd
--- offer/helper_functions.py
+++ offer/helper_functions.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
 
 from offer.models import Offer
@@ -5,3 +6,13 @@ from user.helper_functions import send_notification
 
 
 
+def decline_offer(offer_id):
+    """This function declines the offer with the given offer_id.
+    It sends a notification to the user that made the offer."""
+    offer = get_object_or_404(Offer, pk=offer_id)
+    listing = offer.listing
+    notification_message = 'Your offer on ' + offer.listing.name + ' has been declined'
+    notification_url = redirect('listing-details', listing_id=offer.listing.id)['Location']
+    send_notification(offer.buyer.id, notification_message, notification_url)
+    send_notification(offer.buyer.id, notification_message, notification_url)
+    offer.delete()
