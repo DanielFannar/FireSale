6ab57db63dcafa2b72478d1624314fd9de2d618a
DanielFannar <daniel.fannar@gmail.com>
2022-05-13 21:16:37 +0000

    Fixing decline offer notification function

--- offer/helper_functions.py
+++ offer/helper_functions.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
 from offer.models import Offer
 from user.helper_functions import send_notification
@@ -7,5 +7,6 @@ from user.helper_functions import send_notification
 def decline_offer(offer_id):
     offer = get_object_or_404(Offer, pk=offer_id)
     notification_message = 'Your offer on ' + offer.listing.name + ' has been declined'
-    send_notification(offer.buyer.id, notification_message)
+    notification_url = redirect('listing-details', listing_id=offer.listing.id)
+    send_notification(offer.buyer.id, notification_message, notification_url)
     offer.delete()
